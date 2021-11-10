83ed2b1ac133aa57acb1e4e054f1eb7b595711f5
yanchengv <zqalyc@163.com>
2021-11-10 17:25:57 +0800

    table

--- src/layouts/application.vue
+++ src/layouts/application.vue
@@ -1,20 +1,13 @@
 <template>
-  <el-container style="height: 500px; border: 1px solid #eee">
+  <el-container style="height: 100%; border: 1px solid #eee">
     <side-menu></side-menu>
     <el-container>
-      
        <el-header style="text-align: right; font-size: 12px">
         <web-header></web-header>
       </el-header>
       <el-main>
           <router-view/>
-        <!-- <el-table :data="tableData">
-          <el-table-column prop="date" label="Date" width="140">
-          </el-table-column>
-          <el-table-column prop="name" label="Name" width="120">
-          </el-table-column>
-          <el-table-column prop="address" label="Address"> </el-table-column>
-        </el-table> -->
+          <!-- 添加内容 -->
       </el-main>
     </el-container>
   </el-container>
